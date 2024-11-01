e903870da4b8b3258cb6029f4caab7c08055279d
animeshtheghost <animeshyellow@gmail.com>
2024-11-01 17:46:51 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,24 +5,24 @@ document.addEventListener('DOMContentLoaded', function() {
         return;
     }
     
-   // Function to get a dynamic greeting based on the time of day
-function getGreeting() {
-    const hour = new Date().getHours();
-    if (hour < 12) {
-        return "सुप्रभातम्"; // Good Morning
-    } else if (hour < 13) {
-        return "शुभ मध्यान्हम्"; // Good Noon
-    } else if (hour < 18) {
-        return "शुभ अपराह्णम्"; // Good Afternoon
-    } else if (hour < 20) {
-        return "शुभ सन्ध्या"; // Good Evening
-    } else {
-        return "शुभ रात्रिः"; // Good Night
+    // Function to get a dynamic greeting based on the time of day
+    function getGreeting() {
+        const hour = new Date().getHours();
+        if (hour < 12) {
+            return "सुप्रभातम्"; // Good Morning
+        } else if (hour < 13) {
+            return "शुभ मध्यान्हम्"; // Good Noon
+        } else if (hour < 18) {
+            return "शुभ अपराह्णम्"; // Good Afternoon
+        } else if (hour < 20) {
+            return "शुभ सन्ध्या"; // Good Evening
+        } else {
+            return "शुभ रात्रिः"; // Good Night
+        }
     }
-}
     
     const greeting = getGreeting();
-    const words = [greeting, "ज्ञानम् एव मोक्षः"];
+    const words = [greeting, "ज्ञानम् एव मोक्षः"]; // Knowledge is liberation
     let wordIndex = 0;
     let charIndex = 0;
     const typingDelay = 100;
@@ -50,10 +50,21 @@ function getGreeting() {
 
     typeWord();
 
-    // Add keypress event listener for 'x' and 'X'
+    
     document.addEventListener('keydown', function(event) {
-        if (event.key === 'x' || event.key === 'X') {  // Check for both lowercase 'x' and uppercase 'X'
-            window.location.href = '../codered/codered.html';  // Redirects to codered.html
+        if (event.key === 'x' || event.key === 'X') {  
+            window.location.href = '../codered/easteregg.html'; 
+        }
+    });
+
+
+    let tapCount = 0;
+    document.addEventListener('touchstart', function() {
+        tapCount++;
+        setTimeout(() => { tapCount = 0; }, 500);
+
+        if (tapCount === 3) {
+            window.location.href = '../codered/easteregg.html';
         }
     });
 });
